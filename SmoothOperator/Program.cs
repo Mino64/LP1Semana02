bd485ddbaa8f9a536297a0dd11b5ff5d28830528
Daniel <daniel.henriques.gamedev@gmail.com>
2026-02-25 22:25:21 +0000

    finished ex1

--- SmoothOperator/Program.cs
+++ SmoothOperator/Program.cs
@@ -6,7 +6,11 @@ namespace SmoothOperator
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP!");
+            Console.WriteLine("give int number:");
+            string str = Console.ReadLine();
+            sbyte n = Convert.ToSByte(str);
+            Console.WriteLine($"{--n}\n{++n}");
+
         }
     }
 }
