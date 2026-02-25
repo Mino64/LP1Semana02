157fc29ba052d39cf89e80cbd671e9a53e75a030
Daniel <daniel.henriques.gamedev@gmail.com>
2026-02-25 23:31:43 +0000

    finished ex5

---
+++ VariousOperators/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VariousOperators
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("give a non negative int number");
+            string str = Console.ReadLine();
+            byte n = Convert.ToByte(str);
+
+            Console.WriteLine($"{n / 2}");
+            Console.WriteLine($"{n<<3}");
+            Console.WriteLine($"{n^6}");
+            Console.WriteLine($"{n>10}");
+        }
+    }
+}
