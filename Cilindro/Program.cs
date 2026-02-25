bd5678cdbe607a06a312fe8b7395403f6e9b74b5
Daniel <daniel.henriques.gamedev@gmail.com>
2026-02-25 23:46:46 +0000

    Cylinder exercice done

---
+++ Cilindro/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace Cilindro
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("give cylinder height");
+            string str = Console.ReadLine();
+            double a = Convert.ToDouble(str);
+
+            Console.WriteLine("give cylinder radius");
+            string strRad = Console.ReadLine();
+            double r = Convert.ToDouble(strRad);
+            
+            Console.WriteLine($"V= {MathF.PI*MathF.Pow((float)r,2)*a}");
+            Console.WriteLine($"S= {2*MathF.PI*r*(r+a)}");
+        }
+    }
+}
