00c057785427392abc64ad62480127ca68249962
Daniel <daniel.henriques.gamedev@gmail.com>
2026-02-26 16:14:32 +0000

    added C and F question

---
+++ TempConvert/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace TempConvert
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+            Console.Write("Temperature: ");
+            string tempNum = Console.ReadLine();
+            Console.Write("Unit(C/F): ");
+            string tempUnit = Console.ReadLine();
+            
+
+
+        }
+    }
+}
