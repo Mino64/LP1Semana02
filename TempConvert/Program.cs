52643acff1c37bf8ffd8a338251e7ac3aa6695f5
Daniel <daniel.henriques.gamedev@gmail.com>
2026-02-26 16:21:10 +0000

    added switch case

--- TempConvert/Program.cs
+++ TempConvert/Program.cs
@@ -12,8 +12,19 @@ namespace TempConvert
             string tempNum = Console.ReadLine();
             Console.Write("Unit(C/F): ");
             string tempUnit = Console.ReadLine();
-            
+            float temp = Convert.ToSingle(tempNum);
 
+            switch (tempUnit)
+            {
+                case "C":
+                Console.WriteLine($"{temp:f2} C = {temp*1.8+32:f2} F");
+                break;
+
+                case "F":
+                Console.WriteLine($"{temp:f2} F = {(temp-32)/1.8:f2} C");
+                break;
+
+            }
 
         }
     }
