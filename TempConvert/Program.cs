33e54921e63cf9c13028068249ed46d7e76deff0
Daniel <daniel.henriques.gamedev@gmail.com>
2026-02-26 16:26:11 +0000

    finished TempConvert exercice

--- TempConvert/Program.cs
+++ TempConvert/Program.cs
@@ -18,10 +18,16 @@ namespace TempConvert
             {
                 case "C":
                 Console.WriteLine($"{temp:f2} C = {temp*1.8+32:f2} F");
+                Console.WriteLine($"Absolute value: {Math.Abs(temp*1.8+32):f2}");
                 break;
 
                 case "F":
                 Console.WriteLine($"{temp:f2} F = {(temp-32)/1.8:f2} C");
+                Console.WriteLine($"Absolute value: {Math.Abs((temp-32)/1.8):f2}");
+                break;
+
+                default:
+                Console.WriteLine("Invalid unit.");
                 break;
 
             }
